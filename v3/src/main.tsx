d61d6209e0acdc43363449306eeb7fd66259832c
cory.murphy <cory.murphy@ezcater.com>
2023-02-24 10:14:15 -0500

    more cleanup

--- v3/src/main.tsx
+++ v3/src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
 import 'react-bootstrap';
-
 
 import '@fortawesome/react-fontawesome';
 import 'react-popper';
@@ -16,9 +14,6 @@ import "./css/gallery-grid.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* <BrowserRouter>
-      <App />
-    </BrowserRouter> */}
     <HashRouter>
       <App />
     </HashRouter>
