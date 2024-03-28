f13020c86fd7e21abb42856207f828d2e9453cd9
cory.murphy <cory.murphy@ezcater.com>
2024-03-28 12:33:22 -0400

    update events for 2024

--- site/src/data/events.tsx
+++ site/src/data/events.tsx
@@ -25,46 +25,46 @@ export function getEvents(): ClubEvent[] {
     {
       type: "autocross",
       name: "#1",
-      startDate: "2023-04-15T00:00:00.000",
+      startDate: "2024-04-20T00:00:00.000",
     },
     {
       type: "autocross",
       name: "#2",
-      startDate: "2023-05-14T00:00:00.000",
+      startDate: "2024-06-02T00:00:00.000",
     },
     {
       type: "autocross",
       name: "#3",
-      startDate: "2023-06-17T00:00:00.000",
+      startDate: "2024-06-22T00:00:00.000",
     },
     {
       type: "autocross",
       name: "#4",
-      startDate: "2023-07-23T00:00:00.000",
+      startDate: "2024-07-28T00:00:00.000",
     },
     {
       type: "autocross",
       name: "#5",
-      startDate: "2023-09-02T00:00:00.000",
+      startDate: "2024-08-31T00:00:00.000",
     },
     {
       type: "autocross",
       name: "#6",
-      startDate: "2023-10-07T00:00:00.000",
+      startDate: "2024-10-12T00:00:00.000",
     },
     {
       type: "autocross",
       name: "#7",
-      startDate: "2023-10-22T00:00:00.000",
+      startDate: "2024-10-27T00:00:00.000",
     },
   ];
 }
 
 export function defaultMembership(): MsrEvent {
   return {
-    detailuri:  "https://www.MotorsportReg.com/events/ne-svt-2023-membership-devens-airfield-785736?utm_source=apis&utm_medium=apim&utm_campaign=apic&utm_content=json",
-    name: "NE-SVT 2023 Membership",
-    start: "2023-01-01",
+    detailuri:  "https://www.motorsportreg.com/events/ne-svt-2024-membership-devens-airfield-701754?utm_source=apis&utm_medium=apim&utm_campaign=apic&utm_content=json",
+    name: "NE-SVT 2024 Membership",
+    start: "2024-01-01",
     type: "Membership"
   }
 }
